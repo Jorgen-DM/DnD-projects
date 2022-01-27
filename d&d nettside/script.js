fffc1687a52d1fc6926e4defe5bb083ae58c1e72
Jorgen-DM <98276681+Jorgen-DM@users.noreply.github.com>
2022-01-27 13:51:11 +0100

    Endret utseende++ på hovedsiden.

---
+++ d&d nettside/script.js
@@ -0,0 +1,10 @@
+/* adding and removing the "responsive" class to topnav when user clicks menu icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
